afdad7653efc209a41da709b3085821b40ccd5ee
Ayesha <ayeshamunawarofficial@gmail.com>
2022-04-08 17:29:22 +0500

    ETS> Add header to the page and scroll to tracking  area
    ETS> Add header to the page and scroll to tracking  area when button is clicked

---
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExpenseItem(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ExpenseItem;
---
+++ src/Container/ExpenseTracker/ExpenseTracker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ExpenseTracker(props) {
+    return (
+        <div id="expense-tracker">
+            <h1>Tracking area</h1>
+        </div>
+    );
+}
+
+export default ExpenseTracker;
