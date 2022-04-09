e8b0e84d8cabea510610c06431564e20020bf8ee
Ayesha <ayeshamunawarofficial@gmail.com>
2022-04-09 16:48:23 +0500

    EMS> Create a wrapping component card
    EMS> Create a wrapping component card

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import "./ExpenseItem.css"
 import DateBox from "../DateBox/DateBox";
+import Card from "../../Container/Card/Card";
 
 function ExpenseItem(props) {
     const {date, expenseTitle, amount} = props;
 
     return (
-        <div className="expense-item">
-            <DateBox date={date}/>
-            <div className="expense-item__description">
-                <div className="expense-item__title"><h3 className="heading">{expenseTitle}</h3></div>
-                <div className="paragraph expense-item__price"><h3>{`$ ${amount.toLocaleString()}`}</h3></div>
+        <Card>
+            <div className="expense-item">
+                <DateBox date={date}/>
+                <div className="expense-item__description">
+                    <div className="expense-item__title"><h3 className="heading">{expenseTitle}</h3></div>
+                    <div className="paragraph expense-item__price"><h3>{`$ ${amount.toLocaleString()}`}</h3></div>
+                </div>
             </div>
-        </div>
+        </Card>
     );
 }
 
