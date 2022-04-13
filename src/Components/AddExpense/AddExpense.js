1a94ae39e74a9d1447133b36a34f68c69a501db2
Ayesha <ayeshamunawarofficial@gmail.com>
2022-04-13 17:24:44 +0500

    EMS> On form submit send data all the way back to app
    EMS> On form submit send data all the way back to app component from add expense form

--- src/Components/AddExpense/AddExpense.js
+++ src/Components/AddExpense/AddExpense.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Card from "../../Container/Card/Card";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 import "./AddExpense.css"
 
 function AddExpense(props) {
+    const {onSaveExpense} = props
     return (
         <div className="add-expense-area">
             <div className="add-expense">
-                <ExpenseForm/>
+                <ExpenseForm onSaveExpense={onSaveExpense}/>
             </div>
         </div>
 
